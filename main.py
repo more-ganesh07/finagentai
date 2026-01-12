8a9a2c7a2ea4ea79e82b3a0042948ab98cfc003d
charudatta-17 <charudatta292@gmail.com>
2026-01-13 00:52:10 +0530

    CORS issue resolved

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("FRONTEND_URL", "http://localhost:3000")],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
